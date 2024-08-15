6196fb5bf80493761ecc61858ceb7293d3d5549b
Ndzi Nfor <ndzinfor@gmail.com>
2024-08-15 23:59:34 +0100

    write append functon to add node to end of list

--- LInkedList.mjs
+++ LInkedList.mjs
@@ -9,4 +9,19 @@ class LinkedList {
     constructor() {
         this.head = null;
     }
+
+    append(data) {
+        // Add a new node at the end of the list
+        const newNode = new Node(data);
+        if (!this.head) {
+            this.head = newNode;
+            return;
+        }
+        let lastNode = this.head;
+        while (lastNode.next) {
+            lastNode = lastNode.next;
+        }
+        lastNode.next = newNode;
+    }
+
 }
